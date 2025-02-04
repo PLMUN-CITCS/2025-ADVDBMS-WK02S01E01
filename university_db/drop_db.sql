bb4009f618f621b411db36968b7556d8edac8a88
Joshua Tigno <tignojoshua@plmun.edu.ph>
2025-02-04 09:29:48 +0800

    Initial Project Files

--- university_db/drop_db.sql
+++ university_db/drop_db.sql
@@ -1,3 +1,3 @@
 -- Step 3: (Optional) Drop the database if needed.  Use with extreme caution!
-USE UniversityDB;
-DROP DATABASE IF EXISTS UniversityDB; -- The IF EXISTS clause prevents an error if the database doesn't exist.
+USE `UniversityDB`;
+DROP DATABASE IF EXISTS `UniversityDB`; -- The IF EXISTS clause prevents an error if the database doesn't exist.
